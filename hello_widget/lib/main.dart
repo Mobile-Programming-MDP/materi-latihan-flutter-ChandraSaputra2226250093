8e2a998e26ba4455a8e0fcc37dc6db870d302457
ChandraSaputra2226250093 <145772827+ChandraSaputra2226250093@users.noreply.github.com>
2023-10-13 18:23:14 +0700

    pisah widget

--- hello_widget/lib/main.dart
+++ hello_widget/lib/main.dart
@@ -10,15 +10,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hallo Widget",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Chandra Saputra"),
-        ),
-        body: const Center(
-          child: Text("Hello Chandra Saputra"),
-        ),
+        title: "Hallo Widget",
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: FristScreen());
+  }
+}
+
+class FristScreen extends StatelessWidget {
+  const FristScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Chandra Saputra"),
+      ),
+      body: const Center(
+        child: Text("Hello Chandra Saputra"),
       ),
     );
   }
